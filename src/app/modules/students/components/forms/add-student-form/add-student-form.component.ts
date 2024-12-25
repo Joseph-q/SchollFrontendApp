e7f4085be2656ef173ec56d4423f8f6d251695d5
A.Joseph <ajosephsanz@gmail.com>
2024-12-24 21:38:33 -0600

    add-student-form gender bugs fixed

--- src/app/modules/students/components/forms/add-student-form/add-student-form.component.ts
+++ src/app/modules/students/components/forms/add-student-form/add-student-form.component.ts
@@ -7,13 +7,7 @@ import {
   signal,
   WritableSignal,
 } from '@angular/core';
-import {
-  FormControl,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -21,21 +15,16 @@ import { MatSelectModule } from '@angular/material/select';
 import { AsyncPipe } from '@angular/common';
 import { provideNativeDateAdapter } from '@angular/material/core';
 
-import { Observable } from 'rxjs';
+import { Observable, Subject, takeUntil } from 'rxjs';
 
 import { StudentsService } from '@core/services/student/students.service';
 import { CourseService } from '@core/services/courses/courses.service';
 import { NotificationService } from '@core/services/notification/notification.service';
-import { atLeastOneRequired } from '@core/validators/Form/AtLeastOneRequired';
 import { CoursesResponse } from '@core/services/courses/interfaces/response/courses.response.interface';
 
-
-import { PhoneNumber } from '@shared/modules/students/components/phone-input/interface/phone-number.interface';
 import { PhoneInputComponent } from '@shared/modules/students/components/phone-input/phone-input.component';
-import { GenderEnum } from '@shared/modules/students/constants/GenderEnum';
 
-import { CreateStudent } from './class/CreateStudent.class';
-
+import { CreateStudentForm } from './class/CreateStudent.class';
 
 @Component({
   selector: 'app-add-student-form',
@@ -57,15 +46,6 @@ import { CreateStudent } from './class/CreateStudent.class';
 })
 export class AddStudentFormComponent {
   @Output() created = new EventEmitter<void>();
-  courses$: Observable<CoursesResponse>;
-
-  constructor(
-    private studentService: StudentsService,
-    private courseService: CourseService,
-    private notificationService: NotificationService
-  ) {
-    this.courses$ = this.courseService.getCourses();
-  }
   formError: WritableSignal<string | null> = signal(null);
   nameError: WritableSignal<string | null> = signal(null);
   lastnameError: WritableSignal<string | null> = signal(null);
@@ -73,65 +53,48 @@ export class AddStudentFormComponent {
   numberError: WritableSignal<string | null> = signal(null);
   courseError: WritableSignal<string | null> = signal(null);
 
-  AddStudentForm = new FormGroup(
-    {
-      name: new FormControl('', [
-        Validators.required,
-        Validators.minLength(2),
-        Validators.maxLength(50),
-      ]),
-      lastname: new FormControl('', [
-        Validators.required,
-        Validators.minLength(2),
-        Validators.maxLength(50),
-      ]),
-      email: new FormControl<string | null>(null, [Validators.email]),
-      phone: new FormControl<PhoneNumber | null>(null),
-      birthday: new FormControl<Date | null>(null),
-      gender: new FormControl<GenderEnum | null>(null),
-      course: new FormControl<number | null>(0, [Validators.required]),
-    },
-    {
-      validators: atLeastOneRequired('email', 'phone'),
-    }
-  );
+  unsubscribe$ = new Subject<void>();
+  courses$: Observable<CoursesResponse>;
+  private student = new CreateStudentForm();
+  public createStudentForm = this.student.Form;
+
+  
+  constructor(
+    private studentService: StudentsService,
+    private courseService: CourseService,
+    private notificationService: NotificationService
+  ) {
+    this.courses$ = this.courseService.getCourses();
+  }
 
   OnCreated() {
     this.created.emit();
   }
 
   OnSubmit() {
-    this.nameError.set(null);
-    this.lastnameError.set(null);
-    this.emailError.set(null);
-    this.numberError.set(null);
-    this.courseError.set(null);
-    this.formError.set(null);
-
-    const user = new CreateStudent(this.AddStudentForm);
-
-    this.nameError.set(user.validName());
-    this.lastnameError.set(user.validLastname());
-    this.emailError.set(user.validEmail());
-    this.numberError.set(user.validPhone());
-    this.courseError.set(user.validCourse());
-
-    if (this.AddStudentForm.errors?.['atLeastOneRequired']) {
+    this.nameError.set(this.student.validName);
+    this.lastnameError.set(this.student.validLastName);
+    this.emailError.set(this.student.validEmail);
+    this.numberError.set(this.student.validPhone);
+    this.courseError.set(this.student.validCourse);
+    if (this.createStudentForm.errors?.['atLeastOneRequired']) {
       console.log('Al menos uno ');
       this.formError.set(
         'Al menos uno de los campos (Email o Teléfono) debe ser completado.'
       );
     }
+    if (this.createStudentForm.invalid) return;
 
-    if (user.IsFormValid()) {
-      this.studentService.createStudent(user.StudentToCreate).subscribe({
-        complete: () => {
+    this.studentService
+      .createStudent(this.student.StudentToCreate)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe({
+        complete:()=>{
           this.notificationService.sendCompleteMessage(
-            `Estudiante ${user.StudentToCreate.name} creado`
+            'Estudiante Creado'
           );
-          this.OnCreated();
-        },
+          this.unsubscribe$.next();
+        }
       });
-    }
   }
 }
--- src/app/modules/students/components/forms/add-student-form/class/CreateStudent.class.ts
+++ src/app/modules/students/components/forms/add-student-form/class/CreateStudent.class.ts
@@ -1,21 +1,27 @@
-import { AbstractControl, FormControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, Validators } from "@angular/forms";
 
-import { CreateStudentRequest } from '@core/services/student/interfaces/request/CreateStudentRequest.interface';
+import { atLeastOneRequired } from "@core/validators/Form/AtLeastOneRequired";
+import {StudentResponse} from "@core/services/student/interfaces/response/StudentResponse.interface";
+import { CreateStudentRequest } from "@core/services/student/interfaces/request/CreateStudentRequest.interface";
 
-import { GenderEnum } from '@shared/modules/students/constants/GenderEnum';
-import { PhoneNumber } from '@shared/modules/students/components/phone-input/interface/phone-number.interface';
+import { PhoneNumber } from "@shared/modules/students/components/phone-input/interface/phone-number.interface";
+import { GenderEnum } from "@shared/modules/students/constants/GenderEnum";
+import { FormatNumberToString, FormatPhoneNumber } from "@shared/modules/students/components/phone-input/functions/format-phone-number";
 
-export interface AddStudentForm {
+
+interface ICreateStudentForm {
   name: FormControl<string | null>;
   lastname: FormControl<string | null>;
   email: FormControl<string | null>;
   phone: FormControl<PhoneNumber | null>;
   course: FormControl<number | null>;
   birthday: FormControl<Date | null>;
-  gender: FormControl<GenderEnum | null>;
+  gender: FormControl<GenderEnum| null>;
+
 }
 
-export class CreateStudent {
+export class CreateStudentForm {
+  private form: FormGroup<ICreateStudentForm>;
   private name: AbstractControl<string | null, string | null> | null;
   private lastname: AbstractControl<string | null, string | null> | null;
   private email: AbstractControl<string | null, string | null> | null;
@@ -27,97 +33,135 @@ export class CreateStudent {
   private birthday: AbstractControl<Date | null, Date | null> | null;
   private gender: AbstractControl<GenderEnum | null, GenderEnum | null> | null;
 
-  constructor(private createStudent: FormGroup<AddStudentForm>) {
-    this.name = this.createStudent.get('name');
-    this.lastname = this.createStudent.get('lastname');
-    this.email = this.createStudent.get('email');
-    this.phone = this.createStudent.get('phone');
-    this.course = this.createStudent.get('course');
-    this.birthday = this.createStudent.get('birthday');
-    this.gender = this.createStudent.get('gender');
+  constructor() {
+    this.form = new FormGroup<ICreateStudentForm>(
+      {
+        name: new FormControl('', [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(50),
+        ]),
+        lastname: new FormControl('', [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(50),
+        ]),
+        email: new FormControl<string | null>(null, [Validators.email]),
+        phone: new FormControl<PhoneNumber | null>(null),
+        birthday: new FormControl<Date | null>(null),
+        gender: new FormControl<GenderEnum | null>(null),
+        course: new FormControl<number | null>(null, [Validators.required]),
+      },
+      {
+        validators: atLeastOneRequired('email', 'phone'),
+      }
+    );
+    this.name = this.form.controls.name;
+    this.lastname = this.form.controls.lastname;
+    this.email = this.form.controls.email;
+    this.phone = this.form.controls.phone;
+    this.course = this.form.controls.course;
+    this.gender = this.form.controls.gender;
+    this.birthday = this.form.controls.birthday;
   }
 
-  get StudentToCreate(): CreateStudentRequest {
-    const formValue = this.createStudent.value;
+  public set Form(s: StudentResponse) {
+    var phone: PhoneNumber | null = null;
+    if (s.number) phone = FormatPhoneNumber(s.number);
 
-    var fullPhoneNumber: string | null = null;
+    this.form.patchValue({
+      name: s.name,
+      lastname: s.lastname,
+      email: s.email,
+      phone: phone,
+      birthday: s.birthday ? new Date(s.birthday) : null,
+      course: s.courses ? s.courses[0].id : 0,
+      gender: s.gender,
+    });
+  }
 
-    const phone = this.phone?.value;
+  public get Form(): FormGroup<ICreateStudentForm> {
+    return this.form;
+  }
 
-    if (phone) {
-      fullPhoneNumber = `${phone!.area}${phone!.exchange}${phone!.subscriber}`;
+  public get validName(): string | null {
+    if (this.name) {
+      if (this.name.invalid) {
+        if (this.name.errors?.['required']) {
+          return 'El nombre es requerido.';
+        } else if (this.name.errors?.['minlength']) {
+          return 'El nombre debe tener al menos 2 caracteres';
+        } else if (this.name.errors?.['maxlength']) {
+          return 'El nombre no debe exceder los 50 caracteres';
+        }
+      }
     }
+    return null;
+  }
+
+  public get validLastName(): string | null {
+    const lastname = this.form.get('lastname');
+    if (lastname) {
+      if (lastname.invalid) {
+        if (lastname.errors?.['required']) {
+          return 'El apellido es requerido.';
+        } else if (lastname.errors?.['minlength']) {
+          return 'El apellido debe tener al menos 2 caracteres';
+        } else if (lastname.errors?.['maxlength']) {
+          return 'El apellido no debe exceder los 50 caracteres';
+        }
+      }
+    }
+    return null;
+  }
+
+  public get validEmail(): string | null {
+    const email = this.form.get('email');
+    if (email) {
+      if (email.invalid) {
+        if (email.errors?.['email']) {
+          return 'El email no es válido.';
+        }
+      }
+    }
+    return null;
+  }
+
+  public get validPhone(): string | null {
+    const phone = this.form.get('phone');
+    if (phone) {
+      if (phone.invalid) {
+        return 'El número de teléfono no es válido.';
+      }
+    }
+    return null;
+  }
+
+  public get validCourse(): string | null {
+    const course = this.form.get('course');
+    if (course) {
+      if (course.invalid) {
+        if (course.errors?.['required']) {
+          return 'El curso es requerido.';
+        }
+      }
+    }
+    return null;
+  }
+
+  public get StudentToCreate(): CreateStudentRequest {
+    let phoneNumber: string | undefined;
+    if (this.phone && this.phone.value)
+      phoneNumber = FormatNumberToString(this.phone.value);
 
     return {
-      name: formValue.name!,
-      lastname: formValue.lastname!,
-      email: formValue.email!,
-      number: fullPhoneNumber ?? undefined,
-      coursesId: [Number(this.course!.value!)],
-      birthday: formValue.birthday!,
-      gender: formValue.gender,
+      name: this.name!.value!,
+      lastname: this.lastname!.value!,
+      email: this.email?.value,
+      number: phoneNumber,
+      coursesId: [this.course!.value!],
+      birthday: this.birthday?.value,
+      gender: this.gender?.value,
     };
-  }
-
-  validName(): string | null {
-    if (this.name?.invalid) {
-      if (this.name.errors?.['required']) {
-        return 'El nombre es requerido.';
-      } else if (this.name.errors?.['minlength']) {
-        return 'El nombre debe tener al menos 2 caracteres';
-      }
-    }
-    return null;
-  }
-
-  validLastname(): string | null {
-    if (this.lastname?.invalid) {
-      if (this.lastname.errors?.['required']) {
-        return 'El apellido es requerido.';
-      } else if (this.lastname.errors?.['minlength']) {
-        return 'El apellido debe tener al menos 2 caracteres.';
-      }
-    }
-    return null;
-  }
-
-  validEmail(): string | null {
-    if (this.email?.invalid) {
-      if (this.email.errors?.['required']) {
-        return 'El email es requerido.';
-      } else if (this.email.errors?.['email']) {
-        return 'Debe ser un email válido.';
-      }
-    }
-    return null;
-  }
-
-  validPhone(): string | null {
-    if (this.phone?.invalid) {
-      if (this.phone.errors?.['minlength']) {
-        return 'El número debe tener al menos 10 dígitos.';
-      }
-    }
-    return null;
-  }
-
-  validCourse(): string | null {
-    if (this.course?.invalid) {
-      return 'Introduce un curso';
-    }
-
-    return null;
-  }
-
-  validBirthday(): string | null {
-    return null;
-  }
-
-  validGender(): string | null {
-    return null;
-  }
-
-  IsFormValid(): boolean {
-    return this.createStudent.valid ?? false;
   }
 }
