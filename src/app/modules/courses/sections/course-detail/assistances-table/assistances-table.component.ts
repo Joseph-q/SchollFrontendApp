4247f635dd1cc6c13dd2f66724aa165087958c00
A.Joseph <ajosephsanz@gmail.com>
2025-01-01 16:50:41 -0600

    Assistance table course-detail paginator added

--- src/app/modules/courses/sections/course-detail/assistances-table/assistances-table.component.ts
+++ src/app/modules/courses/sections/course-detail/assistances-table/assistances-table.component.ts
@@ -1,30 +1,27 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit, signal } from '@angular/core';
 import { MatMenuModule } from '@angular/material/menu';
-import { ActivatedRoute } from '@angular/router';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
-import {
-  filter,
-  map,
-  Observable,
-  of,
-  Subject,
-  switchMap,
-  takeUntil,
-  tap,
-} from 'rxjs';
+
+import { Subject, takeUntil } from 'rxjs';
 
 import { StudentFromCourseAssit } from '@core/services/assitance/interfaces/res/CourseAssistanceResponse';
 import { AssistanceService } from '@core/services/assitance/assistance.service';
-import { DatepickerPopupComponent } from '../../../../../shared/components/datepicker-popup/datepicker-popup.component';
+
+import { DatepickerPopupComponent } from '@shared/components/datepicker-popup/datepicker-popup.component';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-assistances-table',
   standalone: true,
   imports: [
+    //Material
     MatTableModule,
-    MatButtonModule,
     MatMenuModule,
+    MatPaginatorModule,
+    //Angular
+
+    //Shared Components
     DatepickerPopupComponent,
   ],
   templateUrl: './assistances-table.component.html',
@@ -35,8 +32,11 @@ export class AssistancesTableComponent implements OnDestroy {
   public courseAssitances: StudentFromCourseAssit[] = [];
   public totalAssistances: number = 0;
   public courseId: string | null = null;
-  
+
   protected date: Date = new Date();
+
+  pageIndex = signal(0);
+  pageSize = signal(25);
 
   private destroy$ = new Subject<void>();
 
@@ -46,23 +46,54 @@ export class AssistancesTableComponent implements OnDestroy {
     this.courseId = courseId;
     if (!this.courseId) return;
     this.assistanceService
-      .getAssistanceSalon(this.courseId, 1, 50, this.date)
+      .getAssistanceSalon(
+        this.courseId,
+        this.pageIndex() + 1,
+        this.pageSize(),
+        this.date
+      )
       .pipe(takeUntil(this.destroy$))
       .subscribe((v) => {
         this.courseAssitances = v.data.students || [];
-        this.totalAssistances = this.courseAssitances.length;
+        this.totalAssistances = v.metadata.total;
       });
   }
 
   onSelectDate() {
+    this.destroy$.next();
     if (!this.courseId) return;
 
     this.assistanceService
-      .getAssistanceSalon(this.courseId, 1, 50, this.date)
+      .getAssistanceSalon(
+        this.courseId,
+        this.pageIndex() + 1,
+        this.pageSize(),
+        this.date
+      )
       .pipe(takeUntil(this.destroy$))
       .subscribe((v) => {
         this.courseAssitances = v.data.students || [];
-        this.totalAssistances = this.courseAssitances.length;
+        this.totalAssistances = v.metadata.total;
+      });
+  }
+
+  handlePaginator(page: PageEvent) {
+    this.destroy$.next()
+    if(!this.courseId) return;
+
+    this.pageIndex.set(page.pageIndex)
+    this.pageSize.set(page.pageSize)
+
+    this.assistanceService
+      .getAssistanceSalon(
+        this.courseId,
+        this.pageIndex() + 1,
+        this.pageSize(),
+        this.date
+      )
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((v) => {
+        this.courseAssitances = v.data.students || [];
       });
   }
 
